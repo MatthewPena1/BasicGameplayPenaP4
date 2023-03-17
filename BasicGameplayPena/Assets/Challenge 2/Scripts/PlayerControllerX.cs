e62f6f443989a586a1981a1930b9e71d7c7aeda1
MatthewPena1 <724514@student.dusd.net>
2023-03-16 22:14:08 -0700

    Updated Basic Gameplay
    Imported and completed Challenge #2

--- BasicGameplayPena/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ BasicGameplayPena/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private float timer;
+    public float spawnTime = 0.5f;
 
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
+
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && timer > spawnTime)
         {
+            timer = 0f; 
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
         }
     }
